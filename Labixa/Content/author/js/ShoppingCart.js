f1aea00e5ee89b455278804dfae5bb0f98007306
tuanmonkeyy@gmail.com <tuanmonkeyy@gmail.com>
2021-04-26 20:05:44 +0700

    comment explain function

--- Labixa/Content/author/js/ShoppingCart.js
+++ Labixa/Content/author/js/ShoppingCart.js
@@ -1,4 +1,5 @@
-﻿let product = [
+﻿//set product tag for product => set it step by step
+let product = [
     {
         name: "Vòng Đá Thạch Anh",
         tag: "vongdathachanh",
@@ -73,21 +74,25 @@
     },
 ]
 
-
+//Get btn add cart (Please add class for it before start)
 let carts = document.querySelectorAll('.btn-add-cart');
+//Check button if have and when i click it
 for (let i = 0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
         cartNumbers(product[i]);
         totalprice(product[i]);
     })
 };
-// count Add to Cart
+
 function onloadCartNumbers() {
     let producNum = localStorage.getItem('cartNumbers')
+    //load page with span number set
     if (producNum) {
         document.querySelector('.cart .span-number').textContent = producNum
     }
+
 }
+//Count Add to Cart
 function cartNumbers(product) {
 
     let productNum = localStorage.getItem('cartNumbers');
@@ -105,52 +110,69 @@ function cartNumbers(product) {
 
 // Add items to the cart
 function setItems(product) {
+    //Get cartItems in local Storage
     let cartItems = localStorage.getItem('productsInCart');
+    //Because of productsInCart now is string but now we want it string to array Json =>
     cartItems = JSON.parse(cartItems);
+    //Check cartItems 
     if (cartItems != null) {
+        //Check product have tag or not =>
         if (cartItems[product.tag] == undefined) {
+            //Set cartItem with tag =>
             cartItems = {
                 ...cartItems,
                 [product.tag]: product
             }
         }
+        //=> if cartItem in stock => +1 cart in next time
         cartItems[product.tag].inCart += 1;
     }
     else {
+        //=>if not, cart =1 and next time will be +1=> loop if
         product.inCart = 1;
-
+        //=>set cartItems
         cartItems = {
             [product.tag]: product
         }
     }
-
+    //cartItems array to string =>
     localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 //Math price
 function totalprice(product) {
-
+    //get Price in local Storage
     let cartPrice = localStorage.getItem('totalprice');
-
+    //Check cartPrice 
+    //if want to check type of Cart Price, console.log(typeof cartPrice)
     if (cartPrice != null) {
+        //Change cartPrice string to Int
         cartPrice = parseInt(cartPrice);
+        //if cartPrice have => cartPrice + price product next if you add to cart
         localStorage.setItem("totalprice", cartPrice + product.price);
     }
     else {
+        //if cartPrice don't hvae in cart
         localStorage.setItem("totalprice", product.price);
     }
 
 }
+//Display Cart
 function displayCart() {
+    //get Items in Local Storage (if want debug not failed, f12+ Application and clear Local store and add item in Cửa hàng)
     let cartItems = localStorage.getItem('productsInCart');
+    //Get Price in Local Storage
     let cartPrice = localStorage.getItem('totalprice');
     cartItems = JSON.parse(cartItems);
+    //Get Table to display product in cart
     let productTable = document.querySelector(".product-cart-body");
+    //Get Table to display total Cart 
     let basketTable = document.querySelector(".basket")
     if (cartItems && productTable && basketTable) {
         productTable.innerHTML = '';
         basketTable.innerHTML = '';
         Object.values(cartItems).map(item => {
             productTable.innerHTML +=
+                //if want show img, make img.jpg = tag product (tag product in the first line)
                 '<div class="product-cart-body" style="width:100%;max-width:650px;display:flex;justify-content: flex-start;margin: 0 auto"><div style="width:45%"><img src="~/Content/stone/images/stone/' + item.tag + '.jpeg"><span>' + item.name + '</span></div><div class="price" style="width:15%;display:flex;align-items:center">$' + item.price + ',00 </div><div class="quanlity" style="width: 20%; display: flex; align-items: center">' + item.inCart + '</div ><div class="Total" style="width: 20%; display: flex; align-items: center" >$' + item.price * item.inCart + '</div > ';
             basketTable.innerHTML = '<div><h3>THÔNG TIN GIỎ HÀNG</h3></div><div><h4>Tổng giá trị đơn hàng<h4><h5>$' + cartPrice + ',00<h5></div>'
         });
